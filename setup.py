70c7ef445d94d9784cf1a6955c214ddeb6729153
awadhiyatamanna <you@example.com>
2023-06-29 14:20:40 -0700

    second commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name="Gem-Stone",
+version="0.0.1",
+author="Tanu",
+author_email="tamannaawadhiya@gmail.com",
+packages=find_packages(),
+install_requires=["pandas","numpy","flask"]
+
+
+
+
+)
